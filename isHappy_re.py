3b2074386d8ef3feb81e592800963f05c8902b6a
valuntiny <459201296@qq.com>
2019-08-13 14:35:28 -0400

    happy number is easy, just use while

---
+++ isHappy_re.py
@@ -0,0 +1,38 @@
+"""
+Quest:
+    Write an algorithm to determine if a number is "happy".
+    A happy number is a number defined by the following process: Starting with any positive integer,
+    replace the number by the sum of the squares of its digits,
+    and repeat the process until the number equals 1 (where it will stay),
+    or it loops endlessly in a cycle which does not include 1.
+    Those numbers for which this process ends in 1 are happy numbers.
+
+    Example:
+    Input: 19
+    Output: true
+    Explanation:
+    12 + 92 = 82
+    82 + 22 = 68
+    62 + 82 = 100
+    12 + 02 + 02 = 1
+
+Solution:
+    just do while loo[, and use 'loop' to store all the number that appeared before
+"""
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        loop = [n]
+        while n != 1:
+            n = sum([int(i) ** 2 for i in str(n)])
+            if n in loop:
+                return False
+            else:
+                loop.append(n)
+        return True
+
+
+test = Solution()
+n = 2
+print(test.isHappy(n))
