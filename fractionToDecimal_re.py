26fa88ec6016c8b69078352efef7f1cb6e13ee81
valuntiny <459201296@qq.com>
2019-08-19 15:12:16 -0400

    too many things to consider

--- fractionToDecimal_re.py
+++ fractionToDecimal_re.py
@@ -1,4 +1,4 @@
-'''
+"""
 Quest:
     Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
     If the fractional part is repeating, enclose the repeating part in parentheses.
@@ -21,31 +21,34 @@ Solution:
     - negative number
     - overflow (not in python)
     - also might be sth like 0.45(6)
-'''
-
+"""
 
 class Solution:
     def fractionToDecimal(self, numerator, denominator):
-        quotient, remainder = divmod(abs(numerator), abs(denominator))
         sign = "-" if numerator * denominator < 0 else ""
+        quotient, remainder = divmod(abs(numerator), abs(denominator))
         res = [sign + str(quotient)]
-
         if remainder == 0:
             return ''.join(str(x) for x in res)
-        res.append(".")
+        else:
+            res.append(".")
 
         hashmap = {}
-        i = 2
+        i = 2  # the index for decimal digits
         while remainder not in hashmap:
             hashmap[remainder] = i
             i += 1
-            quotient, remainder = divmod(remainder * 10, abs(denominator))
+            quotient, remainder = divmod(abs(remainder) * 10, abs(denominator))
             res.append(quotient)
 
+        # need to cut off the (0) tail
         res.insert(hashmap[remainder], "(")
         res.append(")")
 
         return ''.join(str(x) for x in res).replace("(0)", "")
 
+
 test = Solution()
-print(test.fractionToDecimal(1,2))
+numerator = 1
+denominator = 2
+print(test.fractionToDecimal(numerator, denominator))
